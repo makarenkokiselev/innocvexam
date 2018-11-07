afb0dea9e19f636c5598be3389bd8e8e610a23df
makarenko <prrjavadeveloper@gmail.com>
2018-11-08 00:05:29 +0100

    Initial commit

---
+++ src/main/java/com/pablo/innocvexam/InnocvexamApplication.java
@@ -0,0 +1,12 @@
+package com.pablo.innocvexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnocvexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnocvexamApplication.class, args);
+	}
+}
